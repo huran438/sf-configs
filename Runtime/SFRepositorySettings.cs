9c3f849d874fced595fd5e17ea10df3ab9b7ed2f
i.khurani <huran438@gmail.com>
2023-03-04 01:16:04 +0300

    Updated Settings

--- Runtime/SFRepositorySettings.cs
+++ Runtime/SFRepositorySettings.cs
@@ -1,8 +1,10 @@
-﻿using SFramework.Core.Runtime;
+﻿using System;
+using SFramework.Core.Runtime;
 using UnityEngine;
 
 namespace SFramework.Repositories.Runtime
 {
+    [Serializable]
     public class SFRepositorySettings : SFProjectSettings<SFRepositorySettings>
     {
         public string RepositoriesPath => repositoriesPath;
