65e747edcabd639ae4d8e6dd4069590655e0a17f
i.khurani <huran438@gmail.com>
2024-08-12 12:22:40 +0300

    Fix Id generation bug

--- Runtime/SFConfigNode.cs
+++ Runtime/SFConfigNode.cs
@@ -1,5 +1,6 @@
 using System;
 using Newtonsoft.Json;
+using SFramework.Core.Runtime;
 using UnityEngine;
 using UnityEngine.Scripting;
 using UnityEngine.Serialization;
@@ -12,30 +13,32 @@ namespace SFramework.Configs.Runtime
     {
         public void BuildTree()
         {
-            if (Children == null) return;
-
             if (Parent == null)
             {
                 FullId = Id;
             }
+            
+            if (Children == null) return;
+            
             foreach (var child in Children)
             {
-                var fullId = Parent == null ? $"{Id}/{child.Id}" : $"{Parent.Id}/{Id}/{child.Id}";
+                var fullId = Parent == null ? $"{Id}/{child.Id}" : $"{Parent.FullId}/{Id}/{child.Id}";
                 child.FullId = fullId;
                 child.Parent = this;
                 child.BuildTree();
             }
         }
-        
+
         [FormerlySerializedAs("_name")]
         [SerializeField, JsonIgnore]
         private string _id;
-        
+
         public string Id
         {
             get => _id;
             set => _id = value;
         }
+
         public string FullId { get; set; }
         public ISFConfigNode Parent { get; set; }
         public abstract ISFConfigNode[] Children { get; }
